3295d4ca1e915ca446ac7986dfea68596332a791
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2023-12-02 20:40:08 -0300

    fix: ajust chore: fixing eslint errors in files

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const List = styled.ul`
   display: grid;
-  grid-template-columns:  1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   column-gap: 32px;
   row-gap: 32px;
 `
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -3,8 +3,6 @@ import { TagContainer } from './styles'
 export type Props = {
   children: string
 }
-const Tag = ({ children}: Props) => (
-  <TagContainer>{children}</TagContainer>
-)
+const Tag = ({ children }: Props) => <TagContainer>{children}</TagContainer>
 
 export default Tag
